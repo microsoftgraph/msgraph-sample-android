00c68de82f98c71cee9303a02851518ffaf05c34
Andrew Connell <me@andrewconnell.com>
2018-11-07 06:05:41 -0500

    Removed AAD app ID from final solution, left in by accident

--- Demos/04-add-msgraph/app/src/main/java/com/microsoft/nativeo365calendarevents/Constants.java
+++ Demos/04-add-msgraph/app/src/main/java/com/microsoft/nativeo365calendarevents/Constants.java
@@ -2,5 +2,5 @@ package com.microsoft.nativeo365calendarevents;
 
 public class Constants {
   public static final String[] SCOPES = {"openid", "User.Read", "Calendars.Read"};
-  public static final String CLIENT_ID = "7ee1a64b-ffc0-4fec-bf7c-40da9803c567";
+  public static final String CLIENT_ID = "{{REPLACE_WITH_APP_ID}}";
 }
