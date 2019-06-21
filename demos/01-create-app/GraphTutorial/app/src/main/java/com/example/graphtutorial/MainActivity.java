a9e599b7d7e5c4497b0c3d7e812bd13d3b4e26c2
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-06-21 11:44:55 -0400

    Updated projects with new Android Studio and latest Graph/MSAL

--- demos/01-create-app/GraphTutorial/app/src/main/java/com/example/graphtutorial/MainActivity.java
+++ demos/01-create-app/GraphTutorial/app/src/main/java/com/example/graphtutorial/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
